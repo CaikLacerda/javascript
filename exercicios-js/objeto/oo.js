8b793ddb866324c7b5fc58fe86c9197f8a8ff196
Caik <caiklacerda9@gmail.com>
2023-06-30 17:34:21 -0300

    aula 115

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,29 @@
+// COODIGO NAO EXECUTAVEL
+
+// Procedural  <--- Funcoes que manipulam dados
+processamento(valor1, valor2, valor3)
+
+// OO <--- Dados que dentro deles ha funcoes
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Voce passa a invocar a funcao apartir do dado, ou seja, o objeto
+
+
+// Objetos trazidos do mundo real, necessitam de uma abstracao para que possuam apenas caracteristica uteis para o contexto do software
+
+/*
+    Principios importantes:
+    1. Abstracao
+    2. Encapsulamento
+    3. heranca (prototype)
+    4. polimorfismo
+*/
+
+ 
